d2bbad30a93803d89a555a7cb840620ce0ef212d
RAJASURYA2002 <cecsrajasuryar24@gmail.com>
2023-09-23 21:57:32 +0530

    commit

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function widthChangeCallback(myMediaQuery) {
     mobile.classList.remove("display");
     computer.classList.add("display");
     setTimeout(() => {
-      // window.location.href = "/Com_agromart_mainPage.html";
+      window.location.href = "/Com_agromart_mainPage.html";
     }, 10000);
   }
 }
